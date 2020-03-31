704436e567d86b85331b682319465f2cbc53eedb
kimeunyoung01 <rola70@nate.com>
2020-03-31 16:34:52 +0900

    회원 목록 완료

--- gittet100/src/main/java/com/example/demo/dao/MemberDao.java
+++ gittet100/src/main/java/com/example/demo/dao/MemberDao.java
@@ -4,14 +4,9 @@ public class MemberDao {
 	public void insertMember() {
 		System.out.println("회원을 등록하였습니다.");
 	}
-	
-	public void updateMember() {
-		System.out.println("회원의 정보를 수정하였습니다.");
+		
+	public void list() {
+		System.out.println("회원목록입니다.");
 	}
-	
-	public void deleteMember() {
-		System.out.println("회원의 정보를 수정하였습니다.");
-	}
-	
 	
 }
