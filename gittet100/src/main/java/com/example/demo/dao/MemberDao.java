f063b3f8d8b4b74a068fa27ee8fe001c4b6d3ebc
kimeunyoung01 <rola70@nate.com>
2020-03-31 16:32:43 +0900

    수정, 삭제하였습니다.

--- gittet100/src/main/java/com/example/demo/dao/MemberDao.java
+++ gittet100/src/main/java/com/example/demo/dao/MemberDao.java
@@ -4,4 +4,14 @@ public class MemberDao {
 	public void insertMember() {
 		System.out.println("회원을 등록하였습니다.");
 	}
+	
+	public void updateMember() {
+		System.out.println("회원의 정보를 수정하였습니다.");
+	}
+	
+	public void deleteMember() {
+		System.out.println("회원의 정보를 수정하였습니다.");
+	}
+	
+	
 }
