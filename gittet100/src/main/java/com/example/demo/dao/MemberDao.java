dde1444ec647e3b2e0f625754e6364bc125dab71
kimeunyoung01 <rola70@nate.com>
2020-03-31 17:21:05 +0900

    복원 및 회원관리 완료

--- gittet100/src/main/java/com/example/demo/dao/MemberDao.java
+++ gittet100/src/main/java/com/example/demo/dao/MemberDao.java
@@ -1,6 +1,10 @@
 package com.example.demo.dao;
 
 public class MemberDao {
+	public void list() {
+		System.out.println("회원목록입니다.");
+	}
+	
 	public void insertMember() {
 		System.out.println("회원을 등록하였습니다.");
 	}
