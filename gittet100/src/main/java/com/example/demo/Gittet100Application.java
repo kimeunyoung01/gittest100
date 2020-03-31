2dddb85c0133d2cb572d36c49a1d2d4d249e8f19
kimeunyoung01 <rola70@nate.com>
2020-03-31 13:13:45 +0900

    first commit

---
+++ gittet100/src/main/java/com/example/demo/Gittet100Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gittet100Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gittet100Application.class, args);
+	}
+
+}
---
+++ gittet100/src/main/java/com/example/demo/dao/MemberDao.java
@@ -0,0 +1,7 @@
+package com.example.demo.dao;
+
+public class MemberDao {
+	public void insertMember() {
+		System.out.println("회원을 등록하였습니다.");
+	}
+}
